1a5fd93ab766a3d069b402ea8c4df48f0e598ccd
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-10-20 05:27:26 +0400

    initial

---
+++ Template/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Template
+{
+    abstract class Bread
+    {
+        public abstract void MixIngredients();
+
+        public abstract void Bake();
+
+        public virtual void Slice()
+        {
+            Console.WriteLine("Slicing the " + GetType().Name + " bread!");
+        }
+
+        //Burada template metodumuz make metodudur
+        public void Make()
+        {
+            MixIngredients();
+            Bake();
+            Slice();
+        }
+    }
+    class TwelveGrain : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for 12-Grain Bread.");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
+        }
+    }
+    class Sourdough : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for Sourdough Bread.");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Sourdough Bread. (20 minutes)");
+        }
+    }
+    class WholeWheat : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for Whole Wheat Bread.");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Whole Wheat Bread. (15 minutes)");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+
+        }
+    }
+}
---
+++ Visitor/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Visitor
+{
+    public interface IVisitor
+    {
+        void Visit(HPPrinter hpPrinter);
+        void Visit(LexmarkPrinter lexmarkPrinter);
+    }
+    public class FaxVisitor : IVisitor
+    {
+        public void Visit(HPPrinter hpPrinter)
+        {
+            //...Process
+            Console.WriteLine($"{nameof(HPPrinter)}'dan faks gonderilir...");
+        }
+
+        public void Visit(LexmarkPrinter lexmarkPrinter)
+        {
+            //...Process
+            Console.WriteLine($"{nameof(LexmarkPrinter)}'dan faks gonderilir...");
+        }
+    }
+    public interface IPrinter
+    {
+        //Printerlerde   olan print etme özelliyi.
+        void Print();
+        //Concrete Visitor obyekti  bu Concrete Element obyektini
+        //elaqelendirib, gedisati Concrete Visitor obyektine buraxmagimzi
+        //teskil eden metod.
+        void Accept(IVisitor visit);
+    }
+    public class HPPrinter : IPrinter
+    {
+        public void Print()
+        {
+            //...Process
+            Console.WriteLine($"{nameof(HPPrinter)} print edir...");
+        }
+
+        public void Accept(IVisitor visit)
+            => visit.Visit(this);
+    }
+    public class LexmarkPrinter : IPrinter
+    {
+        public void Print()
+        {
+            //...Process
+            Console.WriteLine($"{nameof(LexmarkPrinter)} print edir...");
+        }
+
+        public void Accept(IVisitor visit)
+            => visit.Visit(this);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HPPrinter hp = new();
+            LexmarkPrinter lexmark = new();
+
+            hp.Print();
+            lexmark.Print();
+
+            IVisitor fax = new FaxVisitor();
+            hp.Accept(fax);
+            lexmark.Accept(fax);
+        }
+    }
+}
